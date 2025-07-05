34221dded15b1ebc67227b1945f9111e04af6d56
trong.phamtranduc@gmail.com <trong.phamtranduc@gmail.com>
2025-07-05 10:50:29 +0700

    update workflow 4

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.send('Hello, World 123!');
 })
 
+app.get('/api/v1/products', (req, res) => {
+    res.json([
+        { id: 1, name: 'Product 1', price: 100 },
+        { id: 2, name: 'Product 2', price: 200 },
+        { id: 3, name: 'Product 3', price: 300 }
+    ]);
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
